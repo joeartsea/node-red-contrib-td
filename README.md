afd85581c56b99ce9b0b1cb5c06dcccc448f0fd6
Atsushi Kojo <artsnet111@gmail.com>
2014-10-21 07:48:05 +0900

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+node-red-node-td
+========================
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to write to <a href="http://www.treasuredata.com/" target="_new">TreasureData</a>.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-node-td
+
---
+++ td.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2014 Atsushi Kojo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function (RED) {
+  'use strict';
+  var td = require('td');
+  var request = require('request');
+
+  function TdNode(n) {
+    RED.nodes.createNode(this, n);
+    this.database = n.database;
+    this.table = n.table;
+  }
+
+  RED.nodes.registerType('td', TdNode, {
+    credentials: {
+      account: { type: 'text' },
+      writekey: { type: 'password' }
+    }
+  });
+
+  function TdOutNode(n) {
+    RED.nodes.createNode(this, n);
+    this.td = n.td;
+    this.database = n.database;
+    this.table = n.table;
+    this.tdConfig = RED.nodes.getNode(this.td);
+
+    if (this.tdConfig) {
+      var node = this;
+      var apiUrl = "http://in.treasuredata.com/js/v3/event/";
+      var credentials = RED.nodes.getCredentials(this.td);
+
+      node.on('input', function (msg) {
+        node.status({ fill: 'blue', shape: 'dot', text: 'sending' });
+        request.post({
+          url: apiUrl + this.database + '/' + this.table,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-TD-WRITE-KEY': credentials.writekey
+          },
+          body: JSON.stringify(msg),
+          json: true
+        }, function (err, response, body) {
+          if (err) {
+            node.error(err.toString());
+            node.status({ fill: 'red', shape: 'ring', text: 'failed' });
+          }
+          node.status({});
+        });
+      });
+    } else {
+      this.error("missing td configuration");
+    }
+  }
+  RED.nodes.registerType("td out", TdOutNode);
+}
